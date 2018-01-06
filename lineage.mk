3226bc110c46a67e3c213e3c8d5e890ab240ded9
Kevin <gsosschneiderle@gmail.com>
2018-01-06 15:12:59 +0100

    Update lineage.mk

--- lineage.mk
+++ lineage.mk
@@ -1,9 +1,8 @@
 $(call inherit-product, device/lge/d855/full_d855.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common AOKP stuff.
 
-PRODUCT_NAME := lineage_d855
+PRODUCT_NAME := aokp_d855
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_DEVICE="g3" \
