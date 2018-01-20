f30ae46bd2693bd28debd2467c382c4e3965f75e
Kevin <gsosschneiderle@gmail.com>
2018-01-20 09:58:11 +0100

    Update and rename lineage.mk to aokp.mk

--- aokp.mk
+++ aokp.mk
@@ -1,9 +1,10 @@
 $(call inherit-product, device/lge/d855/full_d855.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+# Inherit common AOKP stuff
+$(call inherit-product, vendor/aokp/configs/common_full.mk)
+$(call inherit-product, vendor/aokp/configs/telephony.mk)
 
-PRODUCT_NAME := lineage_d855
+PRODUCT_NAME := aokp_d855
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_DEVICE="g3" \
